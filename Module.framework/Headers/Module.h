3070a82b7ca0de08fc2bafdb915cc9e3ad487df5
agvale <agvale@aliyun.com>
2019-06-11 18:51:41 +0800

    add

---
+++ Module.framework/Headers/Module.h
@@ -0,0 +1,19 @@
+//
+//  Module.h
+//  Module
+//
+//  Created by Li Ping on 2019/6/11.
+//  Copyright © 2019 MingChe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Module.
+FOUNDATION_EXPORT double ModuleVersionNumber;
+
+//! Project version string for Module.
+FOUNDATION_EXPORT const unsigned char ModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Module/PublicHeader.h>
+
+
