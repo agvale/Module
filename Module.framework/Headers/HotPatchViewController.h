a8856f6de8b7c52594591afebd2c95fd787a3336
agvale <agvale@aliyun.com>
2019-06-11 17:20:32 +0800

    init

---
+++ Module.framework/Headers/HotPatchViewController.h
@@ -0,0 +1,17 @@
+//
+//  HotPatchViewController.h
+//  Module
+//
+//  Created by Li Ping on 2019/6/11.
+//  Copyright © 2019 MingChe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HotPatchViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
